eca3ef0a8b0738ec9a7f24067e4f62e9f7867516
이슬빈 <@DESKTOP-I4OLJ54>
2021-12-09 15:58:06 +0900

    222

---
+++ movieProject/src/main/java/com/hk/project/OldController.java
@@ -0,0 +1,15 @@
+package com.hk.project;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class OldController {
+
+	@RequestMapping(value="/tttt")
+	@ResponseBody
+	public String tttt() { 
+		return "Hello";
+	}
+}
