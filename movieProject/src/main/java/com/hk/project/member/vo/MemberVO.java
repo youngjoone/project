e16c1f04a328b1bc03a8c69b3d31322239c88103
황나윤 <ghkds@DESKTOP-NNH734C>
2021-12-09 17:40:46 +0900

    MemberVO 수정

--- movieProject/src/main/java/com/hk/project/member/vo/MemberVO.java
+++ movieProject/src/main/java/com/hk/project/member/vo/MemberVO.java
@@ -2,13 +2,14 @@ package com.hk.project.member.vo;
 
 import java.util.Date;
 
-public class memberVO {
+public class MemberVO {
 
 	private String id;
 	private String pw;
+	private Date birth;
 	private String name;
+	private int tel;
 	private String email;
-	private Date joinDate;
 	
 	public String getId() {
 		return id;
@@ -22,11 +23,23 @@ public class memberVO {
 	public void setPw(String pw) {
 		this.pw = pw;
 	}
+	public Date getBirth() {
+		return birth;
+	}
+	public void setBirth(Date birth) {
+		this.birth = birth;
+	}
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
+	}
+	public int getTel() {
+		return tel;
+	}
+	public void setTel(int tel) {
+		this.tel = tel;
 	}
 	public String getEmail() {
 		return email;
@@ -34,16 +47,9 @@ public class memberVO {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	public Date getJoinDate() {
-		return joinDate;
-	}
-	public void setJoinDate(Date joinDate) {
-		this.joinDate = joinDate;
-	}
-	
 	@Override
 	public String toString() {
-		return "memberVO [id=" + id + ", pw=" + pw + ", name=" + name + ", email=" + email + ", joinDate=" + joinDate
-				+ "]";
+		return "MemberVO [id=" + id + ", pw=" + pw + ", birth=" + birth + ", name=" + name + ", tel=" + tel + ", email="
+				+ email + "]";
 	}
 }
