c28ef53a82b55e87f45316ef81e97a14eba21bb6
HKEDU <HKEDU@DESKTOP-E4VRFH5>
2021-12-10 09:18:50 +0900

    1

--- movieProject/src/main/java/com/hk/project/OldController.java
+++ movieProject/src/main/java/com/hk/project/OldController.java
@@ -3,7 +3,7 @@ package com.hk.project;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-//올라가~!!
+//올라가~!!ㅁ
 @Controller
 public class OldController {
 
